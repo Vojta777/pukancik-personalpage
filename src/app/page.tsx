62a07cbd4f0ca489cdd0f97f04032287c381513d
Vojtech Pukancik <vojtechpukancik123@gmail.com>
2024-09-12 10:24:20 +0200

    uvodny commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Types() {
+  return (
+      <Typography variant="h1" gutterBottom>
+        hello world! 001
+      </Typography>
+  );
+}
